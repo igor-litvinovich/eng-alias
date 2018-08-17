fd3be982e03aa4ca864b6f85fc05cd03e003e879
Igor Litvinovich <ihar.litvinovich@itechart-group.com>
2018-08-17 12:02:07 +0300

    EA-2. Configured redux.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import Immutable from 'immutable';
 import registerServiceWorker from './registerServiceWorker';
+import rootReducer from './rootReducer';
 import Router from './router';
+import storeFactory from './helpers/storeFactory';
+import  initialState from './constants/initialState';
+
 import './index.scss';
 
+const immutableInitialState = Immutable.fromJS(initialState);
+const store = storeFactory.create(immutableInitialState, rootReducer);
+
 ReactDOM.render((
-  <BrowserRouter basename="/">
-    <Router/>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter basename="/">
+      <Router/>
+    </BrowserRouter>
+  </Provider>
 ), document.getElementById('root'));
 
 registerServiceWorker();
